a8485f0f3adfe6fcd5e069d59b887b3d3dd18f7a
913165 <tinumistry@gmail.com>
2025-05-22 05:43:19 +0530

    first commit

---
+++ src/main/java/com/example/demo_flight_booking2/DemoFlightBooking2Application.java
@@ -0,0 +1,16 @@
+package com.example.demo_flight_booking2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoFlightBooking2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFlightBooking2Application.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/demo_flight_booking2/DemoFlightBooking2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_flight_booking2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFlightBooking2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
